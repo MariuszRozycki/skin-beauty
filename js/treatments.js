73463046b075882392c41ed5704652f2e4931852
MariuszRozycki <mariansc83@icloud.com>
2023-03-30 12:22:48 +0200

    Add function renderProducts() in treatments.js

---
+++ js/treatments.js
@@ -0,0 +1,24 @@
+const treatmentsContainer = document.querySelector('.wrapper-items--treatments');
+
+function renderAllProducts() {
+  for (let product of products) {
+
+    treatmentsContainer.innerHTML += `
+  <div class="item-content item-content-treatment">
+    <div class="grid-container">
+      <div class="item-content--img">
+        <img src="${product.image}"
+          alt="${product.name}">
+      </div>
+      <div class="h2-wrapper">
+        <h2>${product.name}</h2>
+        <p>${product.excerpt}</p>
+      </div>
+      <a href="treatment-details.html" class="btn btn-treatment">Les mer</a>
+    </div>
+  </div>
+  `;
+  }
+}
+
+renderAllProducts();
