555ac0eab703f3922ccce0a73e28c704029834f5
MariuszRozycki <mariansc83@icloud.com>
2023-04-02 18:01:27 +0200

    Images in products.js changed

--- js/products.js
+++ js/products.js
@@ -4,7 +4,7 @@ const products = [
     id: id,
     name: 'Vippe extensions',
     type: 'Vipper',
-    image: '../images/women-lashes/pexels-tubarones-photography-3065453-1.jpg',
+    image: '../images/lashes1.jpg',
     excerpt: 'Noe for alle kvinner som ønsker seg å få lengre vipper',
     description: '1:1 styling er en klassisk metode som består i å feste en falsk øyevippe til en ekte vippe. Effekten her er veldig      naturlig, og øyet er litt skissert. Synlig her er først og fremst forlengelse, krølling og mørklegging av øyevipper, men det er ingen fortykningseffekt. Det er et godt valg for folk som ønsker å forsiktig understreke utseendet, uten den synlige effekten av falske øyevipper. Det vil fungere spesielt godt med naturlig ganske tykke øyevipper.'
   },
@@ -12,7 +12,7 @@ const products = [
     id: ++id,
     name: 'Bryn',
     type: 'Ansiktsbehandling',
-    image: '../images/face.jpg',
+    image: '../images/eyebrows.jpg',
     excerpt: 'Excerpt om bryn',
     description: 'Dypt rengjørende ansiktsbehandling av huden, som gjøres for hånd for å få huden ren. Behandlingen inkluderer  peeling, rensing av huden, serum, maske og krem etter hudtype.'
   },
