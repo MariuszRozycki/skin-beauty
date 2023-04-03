dba408c90bf4e02c7465110a346605005149df42
MariuszRozycki <mariansc83@icloud.com>
2023-04-03 23:01:49 +0200

    Add treatments in treatments

--- js/products.js
+++ js/products.js
@@ -1,16 +1,29 @@
 let id = 1;
+let dataId = 0;
 const products = [
   {
     id: id,
-    name: 'Vippe extensions',
+    name: 'Vipper',
     type: 'Vipper',
     image: '../images/lashes1.jpg',
     excerpt: 'Noe for alle kvinner som ønsker seg å få lengre vipper',
-    description: '1:1 styling er en klassisk metode som består i å feste en falsk øyevippe til en ekte vippe. Effekten her er veldig      naturlig, og øyet er litt skissert. Synlig her er først og fremst forlengelse, krølling og mørklegging av øyevipper, men det er ingen fortykningseffekt. Det er et godt valg for folk som ønsker å forsiktig understreke utseendet, uten den synlige effekten av falske øyevipper. Det vil fungere spesielt godt med naturlig ganske tykke øyevipper.',
-    treatments: innerHTML = `<ul>
-                    <li>vippe extentions</li>
-                    <li>vippeløft</li>
-                </ul>`,
+    treatments: [
+      {
+        dataId: ++dataId,
+        treatment: `
+      <li class="treatment-name">Vippe extentions</li>
+      <li><p class="treatment-content--description" data-id=${dataId}>1:1 styling er en klassisk metode som består i å feste en falsk øyevippe til en ekte vippe. Effekten her er veldig      naturlig, og øyet er litt skissert. Synlig her er først og fremst forlengelse, krølling og mørklegging av øyevipper, men det er ingen fortykningseffekt. Det er et godt valg for folk som ønsker å forsiktig understreke utseendet, uten den synlige effekten av falske øyevipper. Det vil fungere spesielt godt med naturlig ganske tykke øyevipper.</p></li>
+      <li class="read-more" data-id=${dataId}>Read more &gt;&gt;&gt;</li>
+      `
+      },
+      {
+        dataId: ++dataId,
+        treatment: `
+      <li class="treatment-name">Vippeløft</li>
+      <li><p class="treatment-content--description" data-id=${dataId}>Long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'</p></li>
+      <li class="read-more" data-id=${dataId}>Read more &gt;&gt;&gt;</li>`
+      },
+    ]
   },
   {
     id: ++id,
@@ -19,10 +32,23 @@ const products = [
     image: '../images/eyebrows.jpg',
     excerpt: 'Excerpt om bryn',
     description: 'Dypt rengjørende ansiktsbehandling av huden, som gjøres for hånd for å få huden ren. Behandlingen inkluderer  peeling, rensing av huden, serum, maske og krem etter hudtype.',
-    treatments: innerHTML = `<ul>
-                    <li>øyebryn design med farging</li>
-                    <li>brynlaminering/brow lift</li>
-                </ul>`,
+    treatments: [
+      {
+        dataId: ++dataId,
+        treatment: `
+      <li class="treatment-name">Øyebryn design med farging</li>
+      <li><p class="treatment-content--description" data-id=${dataId}>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of.</p></li>
+      <li class="read-more" data-id=${dataId}>Read more &gt;&gt;&gt;</li>
+      `
+      },
+      {
+        dataId: ++dataId,
+        treatment: `
+      <li class="treatment-name">Brynlaminering</li>
+      <li><p class="treatment-content--description" data-id=${dataId}>Long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'</p></li>
+      <li class="read-more" data-id=${dataId}>Read more &gt;&gt;&gt;</li>`
+      }
+    ]
   },
   {
     id: ++id,
@@ -31,15 +57,63 @@ const products = [
     image: '../images/face.jpg',
     excerpt: 'Excerpt om Ansiktsbehandling m/ultralyd',
     description: 'Ultralydsbehandling er en effektiv og skånsom dyp rengjøring og peeling av huden, som samtidig aktiverer hudens funksjoner.',
-    treatments: `<ul>
-                    <li>dyprens</li>
-                    <li>ansiktsbehandling</li>
-                    <li>ansiktsmassasje</li>
-                    <li>no needle masoterapi</li>
-                    <li>ansiktsbehandling med ultralyd</li>
-                    <li>kjemisk peeling</li>
-                    <li>kavitasjon peeling</li>
-                </ul>`,
+    treatments: [
+      {
+        dataId: ++dataId,
+        treatment: `
+      <li class="treatment-name">Dyprens</li>
+      <li><p class="treatment-content--description" data-id=${dataId}>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of.</p></li>
+      <li class="read-more" data-id=${dataId}>Read more &gt;&gt;&gt;</li>
+      `
+      },
+      {
+        dataId: ++dataId,
+        treatment: `
+      <li class="treatment-name">Ansiktsbehandling</li>
+      <li><p class="treatment-content--description" data-id=${dataId}>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of.</p></li>
+      <li class="read-more" data-id=${dataId}>Read more &gt;&gt;&gt;</li>
+      `
+      },
+      {
+        dataId: ++dataId,
+        treatment: `
+      <li class="treatment-name">Ansiktsmassasje</li>
+      <li><p class="treatment-content--description" data-id=${dataId}>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of.</p></li>
+      <li class="read-more" data-id=${dataId}>Read more &gt;&gt;&gt;</li>
+      `
+      },
+      {
+        dataId: ++dataId,
+        treatment: `
+      <li class="treatment-name">No needle mesoterapi</li>
+      <li><p class="treatment-content--description" data-id=${dataId}>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of.</p></li>
+      <li class="read-more" data-id=${dataId}>Read more &gt;&gt;&gt;</li>
+      `
+      },
+      {
+        dataId: ++dataId,
+        treatment: `
+      <li class="treatment-name">Ansiktsbehandling med ultralyd</li>
+      <li><p class="treatment-content--description" data-id=${dataId}>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of.</p></li>
+      <li class="read-more" data-id=${dataId}>Read more &gt;&gt;&gt;</li>
+      `
+      },
+      {
+        dataId: ++dataId,
+        treatment: `
+      <li class="treatment-name">Kjemisk peeling</li>
+      <li><p class="treatment-content--description" data-id=${dataId}>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of.</p></li>
+      <li class="read-more" data-id=${dataId}>Read more &gt;&gt;&gt;</li>
+      `
+      },
+      {
+        dataId: ++dataId,
+        treatment: `
+      <li class="treatment-name">Kavitasjon peeling</li>
+      <li><p class="treatment-content--description" data-id=${dataId}>Long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'</p></li>
+      <li class="read-more" data-id=${dataId}>Read more &gt;&gt;&gt;</li>`
+      }
+    ]
   },
 ];
 
--- js/treatment-details.js
+++ js/treatment-details.js
@@ -1,25 +1,54 @@
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const productId = params.get("id");
-console.log(productId);
 
 const product = products.find(({ id }) => id == productId);
-console.log(product);
+
+const productTreatments = product.treatments;
+const singleTreatment = productTreatments.map(el => el.treatment);
+
+
+
 
 const itemContentTreatmentDetails = document.querySelector('.item-content-treatment-details');
 
 itemContentTreatmentDetails.innerHTML = `
-      <div class="container">
-        <div class="h2-wrapper">
-          <h2>${product.name}</h2>
+        <div class="container">
+          <div class="h2-wrapper">
+            <h2>${product.name}</h2>
+          </div>
+          <div class="item-content--img">
+            <img src="${product.image}"
+              alt="${product.name}">
+          </div>
+          <ul class="treatments-list">
+         ${singleTreatment}
+          </ul>
+          <a href="https://timma.no/salong/alena-beauty" class="btn btn-treatment">Bestill time</a>
         </div>
-        <div class="item-content--img">
-          <img src="${product.image}"
-            alt="${product.name}">
-        </div>
-        ${product.treatments}
-        <p class="item-content--description treatment-content--description">${product.description}</p>
-        <a href="https://timma.no/salong/alena-beauty" class="btn btn-treatment">Bestill time</a>
-      </div>
-`;
+  `;
+
+
+const readMoreButtons = document.querySelectorAll(".read-more");
+let flag;
+
+for (let readMoreButton of readMoreButtons) {
+  readMoreButton.addEventListener('click', () => {
+    flag = !flag;
+    console.log(flag);
+    const treatementDescriptions = document.querySelectorAll(".treatment-content--description");
+    for (let singleDesc of treatementDescriptions) {
+      if (singleDesc.getAttribute("data-id") === (readMoreButton.getAttribute("data-id"))) {
+        singleDesc.classList.toggle("animate-more-description");
+        flag ? readMoreButton.innerHTML = "Less content" : readMoreButton.innerHTML = "Read more";
+      };
+    }
+  });
+}
+
+
+
+
+
+
 
