8d9a58029838caaeb194942eddc00aff29e04317
MariuszRozycki <mariansc83@icloud.com>
2023-04-11 16:55:13 +0200

    Footer hash changed

---
+++ js/carousel.js
@@ -0,0 +1,24 @@
+const carouselWrapper = document.querySelector(".carousel-wrapper");
+
+for (let product of products) {
+
+  console.log(product);
+
+  carouselWrapper.innerHTML = `
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="${product.image}" alt="Slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+  `;
+}
